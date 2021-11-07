5dc03299643504edf7024410b40ab1948cdb79ee
Ken Nguyen <kentmn@gmail.com>
2021-11-07 01:35:50 -0800

    first commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config, os
+from decouple import config 
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,9 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-54-153-118-79.us-west-1.compute.amazonaws.com',  '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'ec2-54-153-118-79.us-west-1.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+    ]
 
 
 # Application definition
